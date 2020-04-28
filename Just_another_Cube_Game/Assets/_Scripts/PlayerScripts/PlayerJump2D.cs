00b3894ef29c42dc29db8fad16e74c3df2261aa2
jfoerderer <julien.foerderer@studmail.w-hs.de>
2020-04-28 21:14:39 +0200

    Refactored PlayerJump2D

--- Just_another_Cube_Game/Assets/_Scripts/PlayerScripts/PlayerJump2D.cs
+++ Just_another_Cube_Game/Assets/_Scripts/PlayerScripts/PlayerJump2D.cs
@@ -34,17 +34,23 @@ public class PlayerJump2D : MonoBehaviour
 
     void Update()
     {
-        var keyboard = Keyboard.current;
-        if (!isJumping && isGrounded && .spaceKey.wasPressedThisFrame)
+        if (!isJumping && isGrounded && Input.GetKey(KeyCode.Space))
             jumpInput = true;
 
     }
     // The Physics Calculations, which let's my Player Jump and Rotating
     void FixedUpdate()
     {
+        updateVelocity();
+        updateGrounding();
+        updateJump();
+        fixRotation();
 
+    }
 
-        // Debug.Log(GetComponent<RectTransform>().localRotation.z % 90);
+    private void updateVelocity()
+    {
+
         // Saving the currently Velocity from the Player into an Vector3 variable
         // Changing the Velocity in X direction by 10
         // Putting the changed Vector3 Velocity into my players Velocity
@@ -55,73 +61,58 @@ public class PlayerJump2D : MonoBehaviour
             vel.x = playerSpeed;
             playerRigidbody.velocity = vel;
         }
-        if(playerCollider.IsTouchingLayers(tileMap) && !MyVariableStorage.performsRestart)
-        // Using a Raycast to determine, if my Player is on the a ground
-        //hit = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<BoxCollider2D>().size.y / 2 + 0.21f);
-        //Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y + 0.21f, 0));
-        //if (hit.collider != null && !MyVariableStorage.performsRestart)
-        {
-            isGrounded = true;
-            isJumping = playerRigidbody.velocity.y > 0.0f ? true : false;
-            // Unfreezing the X and Y Rotations because we are on ground!
-            playerRigidbody.constraints = RigidbodyConstraints2D.None;
-
-            // Jumps if you press the "Space" key
-            if (Input.GetKey(KeyCode.Space) && !isJumping)
-            {
-                // Resetting my Jump Velocity to prevent that i add another Velocity in Y direction 
-                // on top of my Jump before the Gravity fully done its job
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
-                // My Jump Height
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, playerJumpHeight);
-                isJumping = true;
-                isGrounded = false;
-            }
-
-
-
-            // Cause to Strange Behaviors on ground the cube does only sets him plane to 99% the last 1% sometimes bugs.
-            // so i manually set him plane
-
-
-            if (transform.rotation.eulerAngles.z % 90 != 0)
-            {
-                unroundedRotation = transform.rotation.eulerAngles;
-                unroundedRotation.z = Mathf.Round(unroundedRotation.z);
-                transform.eulerAngles = unroundedRotation;
-                //if (transform.rotation.eulerAngles.z % 90 <= 1)
-                //{
-                //    a = -(transform.rotation.eulerAngles.z % 90);
-                //    transform.Rotate(0, 0, a);
-                //    transform.rotation.eulerAngles.z = 2f;
-                //}
-                //else if ((transform.rotation.eulerAngles.z + 1) % 90 <= 1)
-                //{
-                //    a = -((transform.rotation.eulerAngles.z + 1) % 90);
-                //    transform.Rotate(0, 0, a);
-                //}
-            }
-
-
-        }
-        else
-        {
-            // Letting my Cube Rotate in Vector3.back direction with speed modifier  1.135f // 2.05
-            // Freezing all my Rotations in X,Y,Z Axis to prevent strange physics behavior
-            
-            transform.Rotate(Vector3.back * 1.535f);
-            playerRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }
-    }
-
-    private void updateVelocity()
-    {
 
     }
 
     private void updateJump()
     {
+        if (jumpInput && !isJumping && isGrounded && !MyVariableStorage.performsRestart)
+        {
+            // Resetting my Jump Velocity to prevent that i add another Velocity in Y direction 
+            // on top of my Jump before the Gravity fully done its job
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+            // My Jump Height
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, playerJumpHeight);
+            isJumping = true;
+            isGrounded = false;
+            jumpInput = false;
+        }
 
+        // Letting my Cube Rotate in Vector3.back direction with speed modifier  1.135f // 2.05
+        // Freezing all my Rotations in X,Y,Z Axis to prevent strange physics behavior
+        if (isJumping)
+        {
+            transform.Rotate(Vector3.back * 1.535f);
+            playerRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+    }
+
+    private void updateGrounding()
+    {
+        if (playerCollider.IsTouchingLayers(tileMap) && !MyVariableStorage.performsRestart)
+        {
+            //Using a Raycast to determine, if my Player is on the a ground
+            //hit = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<BoxCollider2D>().size.y / 2 + 0.21f);
+            //if (hit.collider != null && !MyVariableStorage.performsRestart)
+            isGrounded = true;
+            //isJumping = playerRigidbody.velocity.y > 0.0f || playerRigidbody.velocity.y < 0.0f ? true : false;
+            isJumping = false;
+            // Unfreezing the X and Y Rotations because we are on ground!
+            playerRigidbody.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+
+    private void fixRotation()
+    {
+        // Cause to Strange Behaviors on ground the cube does only sets him plane to 99% the last 1% sometimes bugs.
+        // so i manually set him plane
+        if (transform.rotation.eulerAngles.z % 90 != 0)
+        {
+            unroundedRotation = transform.rotation.eulerAngles;
+            unroundedRotation.z = Mathf.Round(unroundedRotation.z);
+            transform.eulerAngles = unroundedRotation;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
